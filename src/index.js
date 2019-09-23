02b46d5e4063b25f5781772b686e4c66ba8b97ae
Mikhail Shcheglakov <m.shcheglakov@gmail.com>
2019-09-23 20:34:25 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const map = Object.assign(...bracketsConfig.map(([v, k]) => ({ [v]: k })));
+    const stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (map[str[i]] === str[i] && stack[stack.length - 1] === str[i]) stack.pop();
+        else if (map.hasOwnProperty(str[i])) stack.push(str[i]);
+        else if (map[stack.pop()] !== str[i]) return false;
+    }
+
+    if (stack.length !== 0) return false;
+
+    return true;
+};
